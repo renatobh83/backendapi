ae19b7991d287b9368fa76f25a3cf372cfbb6bf8
Renato <neo_rlm@hotmail.com>
2020-08-24 17:51:15 -0300

    sala

--- src/app/models/Salas.js
+++ src/app/models/Salas.js
@@ -1,27 +1,36 @@
 const mongoose = require("../../database/database");
 const { Schema } = require("../../database/database");
 const Horarios = require("./Horarios");
-const SalaSchema = new mongoose.Schema({
-  nome: {
-    type: String,
-    unique: true,
-    uppercase: true,
-    trim: true,
+const SalaSchema = new mongoose.Schema(
+  {
+    nome: {
+      type: String,
+      unique: true,
+      uppercase: true,
+      trim: true,
+    },
+    setorId: {
+      type: Schema.Types.ObjectId,
+      ref: "Setor",
+      required: true,
+    },
+    ativo: {
+      type: Boolean,
+      default: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now() - 3 * 60 * 60 * 1000,
+    },
   },
-  setorId: {
-    type: Schema.Types.ObjectId,
-    ref: "Setor",
-    required: true,
-  },
-  ativo: {
-    type: Boolean,
-    default: true,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now() - 3 * 60 * 60 * 1000,
-  },
-});
+  {
+    writeConcern: {
+      w: "majority",
+      j: true,
+      wtimeout: 1000,
+    },
+  }
+);
 
 const Sala = mongoose.model("Sala", SalaSchema);
 
