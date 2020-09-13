e91b8c6eb2f2e1fe4964e9a3d27f54a7a4fb6b35
Renatobh83 <neo_rlm@hotmail.com>
2020-09-12 23:15:51 -0300

    erroHeroku.1.0

--- src/app/controller/tabelasController.js
+++ src/app/controller/tabelasController.js
@@ -1,58 +1,58 @@
-// const { defaultResponse, erroResponse } = require("../response");
+const { defaultResponse, erroResponse } = require("../response");
 
-// const Tabelas = require("../models/Tabelas");
-// const mongoose = require("../../database/database");
-// const httpStatus = require("http-status");
-// const ObjectId = mongoose.Types.ObjectId;
+const Tabelas = require("./models/Tabelas");
+const mongoose = require("../../database/database");
+const httpStatus = require("http-status");
+const ObjectId = mongoose.Types.ObjectId;
 
-// class TabelasController {
-//   async store(req, res) {
-//     try {
-//       const find = await Tabelas.findOne({ nome: req.body.nome });
-//       if (find) return res.send(erroResponse("Tabela ja cadastrada"));
-//       const tabela = await Tabelas.create(req.body);
-//       res.send(defaultResponse(tabela));
-//     } catch (error) {
-//       res.send(erroResponse(error.message));
-//     }
-//   }
+class TabelasController {
+  async store(req, res) {
+    try {
+      const find = await Tabelas.findOne({ nome: req.body.nome });
+      if (find) return res.send(erroResponse("Tabela ja cadastrada"));
+      const tabela = await Tabelas.create(req.body);
+      res.send(defaultResponse(tabela));
+    } catch (error) {
+      res.send(erroResponse(error.message));
+    }
+  }
 
-//   async index(req, res) {
-//     try {
-//       const tabelas = await Tabelas.find({});
-//       res.send(defaultResponse(tabelas));
-//     } catch (error) {
-//       res.send(erroResponse(error.message));
-//     }
-//   }
+  async index(req, res) {
+    try {
+      const tabelas = await Tabelas.find({});
+      res.send(defaultResponse(tabelas));
+    } catch (error) {
+      res.send(erroResponse(error.message));
+    }
+  }
 
-//   async updateExames(req, res) {
-//     try {
-//       const { _id: id } = req.params;
+  async updateExames(req, res) {
+    try {
+      const { _id: id } = req.params;
 
-//       const response = await Tabelas.updateOne(
-//         { _id: ObjectId(id) },
-//         { $set: { exames: req.body } }
-//       );
+      const response = await Tabelas.updateOne(
+        { _id: ObjectId(id) },
+        { $set: { exames: req.body } }
+      );
 
-//       res.send(defaultResponse("response"));
-//     } catch (error) {
-//       res.send(erroResponse(error.message));
-//     }
-//   }
+      res.send(defaultResponse("response"));
+    } catch (error) {
+      res.send(erroResponse(error.message));
+    }
+  }
 
-//   async deleteProcedimento(req, res) {
-//     const { procedimento } = req.body;
-//     try {
-//       await Tabelas.updateOne(req.params, {
-//         $pull: { exames: { procedimento: procedimento } },
-//       });
+  async deleteProcedimento(req, res) {
+    const { procedimento } = req.body;
+    try {
+      await Tabelas.updateOne(req.params, {
+        $pull: { exames: { procedimento: procedimento } },
+      });
 
-//       res.send(defaultResponse(httpStatus.NO_CONTENT));
-//     } catch (error) {
-//       res.send(erroResponse(error.message));
-//     }
-//   }
-// }
+      res.send(defaultResponse(httpStatus.NO_CONTENT));
+    } catch (error) {
+      res.send(erroResponse(error.message));
+    }
+  }
+}
 
-// module.exports = new TabelasController();
+module.exports = new TabelasController();
