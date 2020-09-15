28b9cd6354bde6c1e6acc8dc6073d2f2cc8d56e9
Renatobh83 <neo_rlm@hotmail.com>
2020-09-15 10:56:25 -0300

    updateMongoConnect

--- src/database/database.js
+++ src/database/database.js
@@ -4,6 +4,7 @@ mongoose.set("useFindAndModify", false);
 mongoose.connect(process.env.MONGO_DB_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  useCreateIndex: true,
 });
 mongoose.Promise = global.Promise;
 
